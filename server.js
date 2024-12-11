86bce3a2c0ea9dacb5c7764d7fc8c094fca08ff4
Abdelrahman Yehia Ibrahim <freeaccforu98@gmail.com>
2024-12-11 16:32:51 +0200

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { dbConnect } from "./DB/connection.js";
 import userRouter from "./src/modules/user/user.router.js";
+import userNewRouter from "./src/modules/userNew/userNew.router.js";
 import dotenv from "dotenv";
-
 dotenv.config();
 import express from "express";
 import cors from "cors";
@@ -10,5 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
+app.use("/userNew", userNewRouter);
 await dbConnect();
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- src/modules/userNew/userNew.schema.js
+++ src/modules/userNew/userNew.schema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const createUserSchema = Joi.object({
   username: Joi.string().min(3).max(30).required(),
   password: Joi.string().min(8).max(100).required(),
-  email: Joi.string().email().required(),
+  Email: Joi.string().email().required(),
   isMale: Joi.boolean().required(),
   age: Joi.number().integer().min(18).required(),
 });
