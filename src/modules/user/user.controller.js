601bc6780e00f299e0017f63356af858d0a8988c
Abdelrahman Yehia Ibrahim <89707107+PISTAfff@users.noreply.github.com>
2025-03-04 05:54:57 +0200

    Update user.controller.js

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -23,7 +23,7 @@ export const getUser = async (req, res, next) => {
 export const sendCode = async (req, res, next) => {
   const user = await User.findOne({ email: req.body.email });
   sendEmail(user.email, "Verfication Code", "This is A Test");
-  res.status(201).json(user);
+  res.status(200).json(user);
 };
 async function sendEmail(to, subject, text) {
   let transporter = nodemailer.createTransport({
