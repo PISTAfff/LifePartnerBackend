3d0fccfa386740e77f8d1d27574f2cddc456819c
adham hassan <adhamhassaner7@gmail.com>
2025-03-17 19:43:22 +0200

    Update user.controller.js

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -3,6 +3,7 @@ export const getAllUsers = async (req, res, next) => {
     const users = await User.find({});
     res.status(200).json(users);
 };
+
 export const addUser = async (req, res, next) => {
     const user = await User.create(req.body);
     res.status(201).json(user);
