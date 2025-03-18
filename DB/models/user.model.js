2e08ee31fefcff8ff75195186e45c829c02a8429
Abdelrahman Yehia Ibrahim <89707107+PISTAfff@users.noreply.github.com>
2025-03-18 06:55:42 +0200

    .

--- DB/models/user.model.js
+++ DB/models/user.model.js
@@ -1,9 +1,17 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-  username: { type: String, unique: true },
+  firstName: String,
+  lastName: String,
   email: { type: String, unique: true },
   password: String,
+  phone: String,
+  age: Number,
+  gender: {
+    type: String,
+    enum: ["male", "female", "dumbell"],
+    default: "male",
+  },
 });
 
 export const User = model("User", userSchema);
--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -5,11 +5,16 @@ export const getAllUsers = async (req, res, next) => {
   res.status(200).json(users);
 };
 export const addUser = async (req, res, next) => {
-  const user = await User.create(req.body);
-  res.status(201).json(user);
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      res.status(400).json("User already exists");
+    } else {
+      const user = await User.create(req.body);
+      res.status(201).json(user);
+    }
 };
 export const getUser = async (req, res, next) => {
-  const user = await User.findOne({ username: req.body.username });
+  const user = await User.findOne({ email: req.body.email });
   if (!user) {
     res.status(404).json("User not found");
   } else {
