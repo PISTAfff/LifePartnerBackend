c5e41a7752b0bc30b586bb1124bf5d1d389d5ffe
Abdelrahman Yehia Ibrahim <89707107+PISTAfff@users.noreply.github.com>
2025-05-13 02:34:29 +0300

    Revert "."
    This reverts commit 0a6d6fe8aae252c9b675336d1906e864ba8c1f91.

--- DB/models/community/like_dislike.model.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const likeDislikeSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-  type: { type: String, enum: ["like", "dislike"], required: true },
-}, { timestamps: true });
-
-export const LikeDislike = model("LikeDislike", likeDislikeSchema); 
--- src/modules/like_dislike/like_dislike.schema.js
+++
@@ -1,4 +0,0 @@
-const postSchema = new Schema ({
-  likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  dislikes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
