e88ff3a4ea079cb84ab8cfa6a2e796d59abb1ac4
Abdelrahman Yehia Ibrahim <89707107+PISTAfff@users.noreply.github.com>
2025-05-13 06:22:52 +0300

    Update excersice.controller.js

--- src/modules/excersices/excersice.controller.js
+++ src/modules/excersices/excersice.controller.js
@@ -1,5 +1,5 @@
 import cloudinary from "cloudinary";
-import { Excersice } from "../../../DB/models/exercise.model";
+import { Excersice } from "../../../DB/models/exercise.model.js";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
